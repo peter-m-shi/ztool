845b2627bd611eeb9b909e543ae7548fbb41cf3d
sp <shipeng_yy@migu.cn>
2017-05-08 11:28:09 +0800

    [石鹏] Fix Version Check Bug

--- check_changes.sh
+++ check_changes.sh
@@ -6,7 +6,8 @@ old=$PWD
 
 cd "$TOOLS_FOLDER"
 
-git fetch git@github.com:PGClient/ztool.git master --quiet >/dev/null 2>&1
+origin=`git ls-remote --get-url origin`
+git fetch $origin master --quiet >/dev/null 2>&1
 
 if [[ $? -eq 0 ]]; then
 	#git log --oneline FETCH_HEAD ^master>$TOOLS_FOLDER/.tools-changes
