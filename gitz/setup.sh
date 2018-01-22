dc2e09885c2791b7faa34ad8c287e46284ed862f
sp <shipeng_yy@migu.cn>
2018-01-22 10:50:29 +0800

    Fix

--- gitz/setup.sh
+++ gitz/setup.sh
@@ -5,19 +5,17 @@ source $HOME/ztool/dependency/profile
 dependency_gem atlassian-stash
 if [[ `uname` -eq "Darwin" ]]; then
 	dependency_brew git-flow-avh
+
+	#添加全局hooks模板
+	GIT_TEMPLATES_DIRECTORY="/Applications/GitHub.app/Contents/Resources/git/share/git-core/templates"
+	cp -r $HOME/ztool/gitz/commit-msg "$GIT_TEMPLATES_DIRECTORY/hooks"
+	cp -r $HOME/ztool/gitz/pre-commit "$GIT_TEMPLATES_DIRECTORY/hooks"
+	git config --global init.templatedir $GIT_TEMPLATES_DIRECTORY
 else
 	curl -OL https://raw.github.com/nvie/gitflow/develop/contrib/gitflow-installer.sh
 	chmod +x gitflow-installer.sh
 	sudo ./gitflow-installer.sh
 fi
-
-
-#添加全局hooks模板
-GIT_TEMPLATES_DIRECTORY="/Applications/GitHub.app/Contents/Resources/git/share/git-core/templates"
-cp -r $HOME/ztool/gitz/commit-msg "$GIT_TEMPLATES_DIRECTORY/hooks"
-cp -r $HOME/ztool/gitz/pre-commit "$GIT_TEMPLATES_DIRECTORY/hooks"
-git config --global init.templatedir $GIT_TEMPLATES_DIRECTORY
-
 
 #安装命令补全
 source $HOME/ztool/completion/profile
