c70d196d50f3cb7bb95874c15ed687725abec9a3
sp <shipeng_yy@migu.cn>
2018-01-29 11:12:17 +0800

    Fix update.sh

--- update.sh
+++ update.sh
@@ -3,13 +3,7 @@
 TOOLS_FOLDER="$HOME/ztool"
 if [ -d $TOOLS_FOLDER ]; then
 	old=$PWD
-
-	if [ $2 = "-s" ]; then
-		cd "$TOOLS_FOLDER" && git pull origin master && sh setup.sh $1
-	else
-		cd "$TOOLS_FOLDER" && git pull origin master
-	fi
-
+	cd "$TOOLS_FOLDER" && git pull origin master && sh setup.sh $1
 	cd $old
 else
 	str="ztool not installed at $HOME/"
